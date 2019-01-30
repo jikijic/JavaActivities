9f0ab2c5f23a7b43b4b877417530db75410d07ee
jikijic <47112916+jikijic@users.noreply.github.com>
2019-01-30 22:00:06 +0800

    Update CarLoan.java

--- CarLoan.java
+++ CarLoan.java
@@ -1,23 +1,28 @@
+/*
+Calculate the monthly car payment a user should expect to make after making out a car loan
+Activity from Codecademy
+Author: Jeric Cereno
+*/
 public class CarLoan { 
-//calculate the monthly car payment a user should expect to make after making out a car loan
-	public static void main(String[] args) {
-		int carLoan = 10000;
-    int loanLength = 3;
-    int interestRate = 5;
-		int downPayment = 2000;
+    public static void main(String[] args) {
+	int carLoan = 10000;
+	int loanLength = 3;
+    	int interestRate = 5;
+	int downPayment = 2000;
     
-    if (loanLength <= 0 || interestRate <= 0) {
-      System.out.println("Error! You must take out a valid car loan.");
-    } else if (downPayment >= carLoan) {
-      System.out.println("The car can be paid in full");
-    } else {
-      int remainingBalance = carLoan - downPayment;
-      int months = loanLength * 12;
-      int monthlyBalance = remainingBalance / months;
-      int interest = (monthlyBalance * interestRate) / 100;
-      int monthlyPayment = monthlyBalance + interest;
-      System.out.println(monthlyPayment);
-    }
+    	if (loanLength <= 0 || interestRate <= 0) {
+      		System.out.println("Error! You must take out a valid car loan.");
+    	} else if (downPayment >= carLoan) {
+      		System.out.println("The car can be paid in full");
+    	} else {
+      		int remainingBalance = carLoan - downPayment;
+      		int months = loanLength * 12;
+      		int monthlyBalance = remainingBalance / months;
+      		int interest = (monthlyBalance * interestRate) / 100;
+      		int monthlyPayment = monthlyBalance + interest;
+      
+		System.out.println(monthlyPayment);
+    	}	
 
-	}
+    }
 }
